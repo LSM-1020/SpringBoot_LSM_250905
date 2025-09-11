23e90b1a5343e470f1bcdc4f699fd5a414fa7365
LSM-1020 <smmlee@naver.com>
2025-09-11 15:04:02 +0900

    검색창 및 검색기능 추가

--- src/main/java/com/LSM/smboard/question/QuestionRepository.java
+++ src/main/java/com/LSM/smboard/question/QuestionRepository.java
@@ -2,6 +2,7 @@ package com.LSM.smboard.question;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -31,6 +32,42 @@ public interface QuestionRepository extends JpaRepository<Question, Integer>{
 	         nativeQuery = true)
 	    List<Question> findQuestionsWithPaging(@Param("startRow") int startRow,
 	                                           @Param("endRow") int endRow);
+	//검색을 조회하는 페이징
+	@Query(value = 
+		       "SELECT * FROM ( " +
+		       "   SELECT q.*, ROWNUM rnum FROM ( " +
+		       "       SELECT DISTINCT q.* " +
+		       "       FROM question q " +
+		       "       LEFT OUTER JOIN siteuser u1 ON q.author_id = u1.id " +
+		       "       LEFT OUTER JOIN answer a ON a.question_id = q.id " +
+		       "       LEFT OUTER JOIN siteuser u2 ON a.author_id = u2.id " +
+		       "       WHERE q.subject LIKE '%'||:kw||'%' " +
+		       "          OR q.content LIKE '%'||:kw||'%'" +
+		       "          OR u1.username LIKE '%'||:kw||'%'" +
+		       "          OR a.content LIKE '%'||:kw||'%'" +
+		       "          OR u2.username LIKE '%'||:kw||'%'" +
+		       "       ORDER BY q.createdate DESC " +
+		       "   ) q WHERE ROWNUM <= :endRow " +
+		       ") WHERE rnum > :startRow", 
+		       nativeQuery = true)
+	 List<Question> searchQuestionsWithPaging(@Param("kw") String kw,@Param("startRow") int startRow,
+             @Param("endRow") int endRow);
+	//검색 결과 총 갯수 반환
+	@Query(value = 
+  
+    "   SELECT COUNT(DISTINCT q.id) " +
+    "       FROM question q " +
+    "       LEFT OUTER JOIN siteuser u1 ON q.author_id = u1.id " +
+    "       LEFT OUTER JOIN answer a ON a.question_id = q.id " +
+    "       LEFT OUTER JOIN siteuser u2 ON a.author_id = u2.id " +
+    "       WHERE q.subject LIKE '%'||:kw||'%' " +
+    "          OR q.content LIKE '%'||:kw||'%'" +
+    "          OR u1.username LIKE '%'||:kw||'%'" +
+    "          OR a.content LIKE '%'||:kw||'%'" +
+    "          OR u2.username LIKE '%'||:kw||'%'",
+           nativeQuery = true)
+	public int countSearchResult(@Param("kw") String kw);
 	
+	//Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 	
 }
--- src/main/java/com/LSM/smboard/question/QuestionService.java
+++ src/main/java/com/LSM/smboard/question/QuestionService.java
@@ -7,12 +7,22 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import com.LSM.smboard.DataNotFoundException;
+import com.LSM.smboard.answer.Answer;
 import com.LSM.smboard.user.SiteUser;
 
 import ch.qos.logback.core.model.Model;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import javassist.SerialVersionUID;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -47,17 +57,23 @@ public class QuestionService {
 	}
 	
 	//페이징 테스트
-		public Page<Question> getPageQuestions(int page) {
+		public Page<Question> getPageQuestions(int page, String kw) {
+			
+			//Specification<Question> spec = search(kw);
 			int size = 10; //1페이지당 10개씩 글 출력
 			
 			int startRow = page * size;
 			int endRow = startRow + size;
 			
+			//검색어 없이 리스트 조회
 			List<Question> pageQuestionList = questionRepository.findQuestionsWithPaging(startRow, endRow);
-			
 			long totalQuestion = questionRepository.count(); //모든 글 갯수 가져오기
 			
-			Page<Question> pagingList = new PageImpl<>(pageQuestionList, PageRequest.of(page, size), totalQuestion);
+			//검색어로 검색결과 리스트 조회
+			List<Question> searchQuestionList = questionRepository.searchQuestionsWithPaging(kw, startRow, endRow);
+			int totalSearchQuestion = questionRepository.countSearchResult(kw);
+			
+			Page<Question> pagingList = new PageImpl<>(searchQuestionList, PageRequest.of(page, size), totalSearchQuestion);
 			
 			return pagingList;
 			
@@ -90,6 +106,32 @@ public class QuestionService {
 	public void hit(Question question) { //조회수 증가questionRepository.updateHit(id);
 		question.setHit(question.getHit()+1);
 		questionRepository.save(question);
+	}
+	
+	private Specification<Question> search(String kw) { //키워드(kw) 검색조회 
+		
+		return new Specification<Question>() {
+			private static final long SerialVersionUID = 1L;
+			
+			@Override
+			public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				query.distinct(true); //distinct->중복 제거
+				Join<Question, SiteUser> u1 = q.join("author",JoinType.LEFT); //question+siteUser테이블과 left조인
+				Join<Question, Answer> a = q.join("answerList",JoinType.LEFT); //question+answer테이블과 조인
+				Join<Answer, SiteUser> u2 = a.join("author",JoinType.LEFT);	//answer+siteUser테이블과 left조인
+				
+				return cb.or(cb.like(q.get("subject"), "%"+kw+"%"), //질문 제목에서 검색어 조회
+						cb.like(q.get("content"), "%"+kw+"%"),//질문 내용에서 검색어 조회
+						cb.like(u1.get("username"), "%"+kw+"%"),//질문 작성자에서 검색어 조회
+						cb.like(a.get("content"), "%"+kw+"%"),//답변 내용에서 검색어 조회
+						cb.like(u2.get("username"), "%"+kw+"%")//답변 작성자에서 검색어 조회
+						) 
+					
+						;
+			}
+		};
+		
+		
 	}
 	
 }
