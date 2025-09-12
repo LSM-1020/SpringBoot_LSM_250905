ac2d7caf595ee712641213e6d34a34fd5cff4f0d
LSM-1020 <smmlee@naver.com>
2025-09-12 15:45:01 +0900

    오라클 클라우드용 수정

--- src/main/java/com/LSM/smboard/LsmProjectApplication.java
+++ src/main/java/com/LSM/smboard/LsmProjectApplication.java
@@ -2,9 +2,17 @@ package com.LSM.smboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LsmProjectApplication {
+public class LsmProjectApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(LsmProjectApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LsmProjectApplication.class, args);
--- src/main/java/com/LSM/smboard/MainController.java
+++ src/main/java/com/LSM/smboard/MainController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-	@GetMapping(value = "/") //root 요청 처리
+	@GetMapping(value = "/") //오라클 root 요청 처리
+	//@GetMapping(value = "/") // 로컬용 root 요청 처리
 	public String root() {
 		return "redirect:/question/list";
 	}
